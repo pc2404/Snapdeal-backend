44ba2b14898f4f7fe6c145561d52f755a37a175f
Priyanka Chaurasia <pc24priyanka@gmail.com>
2023-02-25 20:27:48 +0530

    backend complete

---
+++ data/index.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userIds = [
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+];
+
+export const users = [
+  {
+    _id: userIds[0],
+    firstName: "test",
+    lastName: "me",
+    email: "aaaaaaa@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    createdAt: 1115211422,
+    updatedAt: 1115211422,
+    __v: 0,
+  },
+  {
+    _id: userIds[1],
+    firstName: "Steve",
+    lastName: "Ralph",
+    email: "thataaa@gmail.com",
+    password: "$!FEAS@!O)_IDJda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    createdAt: 1595589072,
+    updatedAt: 1595589072,
+    __v: 0,
+  },
+  {
+    _id: userIds[2],
+    firstName: "Some",
+    lastName: "Guy",
+    email: "someguy@gmail.com",
+    password: "da39a3ee5e6b4b0d3255bfef95601890afd80709",
+    createdAt: 1288090662,
+    updatedAt: 1288090662,
+    __v: 0,
+  },
+];
---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+
+import User from "./models/User.js";
+import { users } from "./data/index.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+// ---mongoose setup
+
+const PORT = process.env.PORT || 6002;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
